48b9b4c8b2dd896f5c2ce10288664543726dff21
coffee100percnt <antiendershow221@gmail.com>
2021-12-13 16:42:15 +0200

    THIS VERSION OF BOT REQUIRES PYCORD 2.0

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='*')
-guild = discord.Guild
+bot = discord.Bot()
 
 token = 'ODYxNTc0MzIwNjgxMzg1OTg0.YOLxnQ.LTsffwXW2bWhzIZagq7hHgYmTdM'
 def check_if_it_is_me(ctx):
@@ -16,29 +15,26 @@ async def on_command_error(ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send("а аргументы.")
 
-@bot.event
-async def on_ready():
-    bot.add_command(name="ban", description="Банит участника. ```*ban <user> [reason]```")
-    bot.add_command(name="kick", description="Кикает участника. ```*kick <user>```")
 
 
-@bot.command()
+
+@bot.slash_command()
 @commands.has_permissions(ban_members = True)
-async def ban(ctx, usr, *, reasn='пошёл нахуй'):
+async def ban(ctx, usr:discord.Member, *, reasn='пошёл нахуй'):
     async with ctx.typing():
         await usr.send(f"Вы были забанены с {ctx.guild.name} по причине {reasn}")
-        await ctx.guild.ban(user=usr, reason=f"{reasn} ({ctx.author.name})")
+        await usr.ban(user=usr, reason=f"{reasn} ({ctx.author.name})")
         await ctx.send('Ок, забанил')
 
-@bot.command()
+@bot.slash_command()
 @commands.has_permissions(kick_members = True)
-async def kick(ctx, usr):
+async def kick(ctx, usr:discord.Member):
     async with ctx.typing():
         await usr.send(f"Вы были кикнуты с {ctx.guild.name}")
         await ctx.guild.kick(user=usr)
         await ctx.send('Ок, кикнул')
 
-@bot.command()
+@bot.slash_command()
 @commands.check(check_if_it_is_me)
 async def cock(ctx, *, command):
     await eval(command)
