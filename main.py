f5311496871284d63001a68f0fddbacd926294ef
Dima <antiendershow221@gmail.com>
2023-12-15 19:49:58 +0100

    kdeuser 1.0 release

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import discord
 from discord.ext import commands
 import os
+import datetime
+import pandas
 
 bot = discord.Bot()
-
 token = os.getenv('API_TOKEN')
+
 def check_if_it_is_me(ctx):
     return ctx.message.author.id == 335102389017378818
+def torelativetime(interval):
+    a = interval.replace("d", " днів ")
+    b = a.replace("w", " тижнів ")
+    c = b.replace("h", " годин ")
+    d = c.replace("m", " хвилин ")
+    return d
 
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
         await ctx.send("лох без прав)))")
-    if isinstance(error, commands.MissingRequiredArgument):
+    elif isinstance(error, commands.MissingRequiredArgument):
         await ctx.send("аргументи де")
 
 @bot.event
@@ -30,9 +38,9 @@ async def on_ready():
 
 @bot.slash_command()
 @commands.has_permissions(ban_members = True)
-async def ban(ctx, member:discord.Member, *, reason='іді нахуй'):
+async def ban(ctx, member:discord.Member, reason='іді нахуй'):
     await member.ban(reason=f"{reason} ({ctx.author.name})")
-    await ctx.send(f'Вигнав {member.name} з сервера.')
+    await ctx.respond(f'{member.name} вигнано з сервера.')
     await ctx.create_dm(member.id)
     await member.send(f"Ви були вигнані з {ctx.guild.name} з причини {reason}")
 
@@ -40,37 +48,33 @@ async def ban(ctx, member:discord.Member, *, reason='іді нахуй'):
 @commands.has_permissions(kick_members = True)
 async def kick(ctx, member:discord.Member):
     await member.kick()
-    await ctx.send(f'Вигнав {member.name} з сервера.')
+    await ctx.respond(f'{member.name} вигнано з сервера.')
     await ctx.create_dm(member.id)
     await member.send(f"Ви були вигнані {ctx.guild.name}")
 
 @bot.slash_command()
 @commands.has_permissions(ban_members = True)
 async def unban(ctx, id):
-<<<<<<< HEAD
-    banned_users = await ctx.guild.bans()
-    for ban_entry in banned_users:
-        user = ban_entry.user
-        if user.id == id:
-            ctx.guild.unban(user)
-=======
     membr = await ctx.fetch_member(id)
     await membr.unban()
-    await ctx.send(f"{membr.name} може повертатись на сервер.")
->>>>>>> c512cf1 (translation to ukrainian)
+    await ctx.respond(f"{membr.name} може повертатись на сервер.")
 
-# @bot.slash_command()
-# @commands.has_permissions()
+@bot.slash_command()
+@commands.has_permissions(moderate_members = True)
+async def mute(ctx, member:discord.Member, time="10m", reason=None):
+    ttime = pandasd.Timedelta(time).to_pytimedelta()
+    await member.timeout_for(ttime, reason)
+    await ctx.respond(f"{member.name} посидить {torelativetime(time)}без права голосу")
+
+@bot.slash_command()
+@commands.has_permissions(moderate_members = True)
+async def unmute(ctx, member:discord.Member):
+    await member.remove_timeout()
+    await ctx.respond(f"{member.name} повернено право голосу.")
 
 # @bot.command()
 # @commands.check(check_if_it_is_me)
 # async def cock(ctx, *, command):
 #     await eval(command)
-<<<<<<< HEAD
-    
-bot.run(token)
-
-=======
 
 bot.run(str(token))
->>>>>>> c512cf1 (translation to ukrainian)
